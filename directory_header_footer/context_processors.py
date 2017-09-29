7e5bdcabbb838d71b9f9bed597a35e64a2d908cf
richtier <rikatee@gmail.com>
2017-09-29 10:22:43 +0100

    Rename SSO_* env vars to SSO_PROXY

--- directory_header_footer/context_processors.py
+++ directory_header_footer/context_processors.py
@@ -3,13 +3,14 @@ from django.conf import settings
 
 def sso_processor(request):
     url = request.build_absolute_uri()
+    login_url = settings.SSO_PROXY_LOGIN_URL
     return {
         'sso_user': request.sso_user,
         'sso_is_logged_in': request.sso_user is not None,
-        'sso_login_url': '{0}?next={1}'.format(settings.SSO_LOGIN_URL, url),
-        'sso_register_url': settings.SSO_SIGNUP_URL,
-        'sso_logout_url': settings.SSO_LOGOUT_URL,
-        'sso_profile_url': settings.SSO_PROFILE_URL,
+        'sso_login_url': '{0}?next={1}'.format(login_url, url),
+        'sso_register_url': settings.SSO_PROXY_SIGNUP_URL,
+        'sso_logout_url': settings.SSO_PROXY_LOGOUT_URL,
+        'sso_profile_url': settings.SSO_PROXY_PROFILE_URL,
     }
 
 
