42dad3c5fe518d6c31f3534c01a037c165bfc3a4
richtier <rikatee@gmail.com>
2017-11-29 17:09:34 +0000

    Add test for staticfiles

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,8 @@
+import pytest
+
+from django.core.management import call_command
+
+
 def pytest_configure():
     from django.conf import settings
     settings.configure(
@@ -8,13 +13,25 @@ def pytest_configure():
         SSO_PROXY_LOGOUT_URL='http://logout.com',
         SSO_PROFILE_URL='http://profile.com',
         INSTALLED_APPS=[
+            'django.contrib.staticfiles',
             'directory_constants',
             'directory_header_footer',
         ],
+        STATIC_URL='/static',
+        STATIC_ROOT='staticfiles/',
+        STATICFILES_STORAGE='whitenoise.storage.ManifestStaticFilesStorage',
         HEADER_FOOTER_CONTACT_US_URL='http://contact-us.com',
         TEMPLATES=[{
             'BACKEND': 'django.template.backends.django.DjangoTemplates',
             'APP_DIRS': True,
         }],
+        MIDDLEWARE_CLASSES=[
+            'whitenoise.middleware.WhiteNoiseMiddleware',
+        ],
         HEADER_FOOTER_CSS_ACTIVE_CLASSES={'fab': True},
     )
+
+
+@pytest.fixture(autouse=True)
+def call_collectstatic():
+    call_command('collectstatic', interactive=False)
