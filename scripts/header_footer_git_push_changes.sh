6e6c775cdea5170394c078dc5050138065fda426
eadpearce <e.pearce@tutanota.com>
2018-01-23 12:38:39 +0000

    Added hub commands to script to automate creation of PRs

--- scripts/header_footer_git_push_changes.sh
+++ scripts/header_footer_git_push_changes.sh
@@ -6,11 +6,13 @@ REPOS="
 ../directory-ui-buyer
 ../directory-ui-export-readiness
 ../navigator"
-IFS= read -r -p "Enter commit message: " commitmsg
+IFS= read -r -p "	Enter commit message: " commitmsg
+IFS= read -r -p "	Enter JIRA ticket number (e.g. ED-0000): " ticketnum
 for dir in $REPOS; do
-	echo "Switching to repo $dir"
+	echo "	Switching to repo $dir"
 	cd $dir
 	git add -A
 	git commit -m "$commitmsg"
 	git push -u origin $(git rev-parse --abbrev-ref HEAD)
+	git pull-request -b master -m "https://uktrade.atlassian.net/browse/$ticketnum"
 done
